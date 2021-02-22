7172786671af3b430cb465d9724efbd243a351a6
littledumpleng <73508837+littledumpleng@users.noreply.github.com>
2021-02-22 14:42:18 -0500

    Added script3.js

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,30 @@
-/* Put your javascript in here */
+/* configuration */
+
+console.log('hello'); /*console.log() shows up in console, alert() shows up in browser - use to check javascript connection*/
+
+
+const carousel = document.body.querySelector('.carousel');
+
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0; // ribbon scroll position
+
+carousel.querySelector('.left').onclick = function() {
+  // shift left
+  position += width * count;
+  // can't move to the left too much, end of images
+  position = Math.min(position, 0)
+  list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.right').onclick = function() {
+  // shift right
+  position -= width * count;
+  // can only shift the ribbbon for (total ribbon length - visible count) images
+  position = Math.max(position, -width * (listElems.length - count));
+  list.style.marginLeft = position + 'px';
+};
---
+++ public/labs/lab_3/script3.js
@@ -0,0 +1,28 @@
+const listContainer = document.querySelector('images')
+
+function shiftLeft(){
+    const lastThree = Array.from(listContainer.children).slice(4,7).reverse();
+    lastThree.forEach((element) => {
+        listContainer.removeChild(element);
+        listContainer.insertBefore(element, listContainer.children[0]);
+    })
+}
+
+function shiftRight(){
+    const firstThree = Array.from(listContainer.children).slice(0,3);
+    firstThree.forEach((element) => {
+        listContainer.removeChild(element);
+        listContainer.appendChild(element);
+    })
+}
+
+function onLoadOfPage(){
+    document.querySelector("button.arrow.left").addEventListener('click', (event) =>{
+        shiftLeft();
+    });
+    document.querySelector("button.arrow.right").addEventListener('click', (event) =>{
+        shiftRight();
+    })
+}
+
+window.onload = onLoadOfPage;
