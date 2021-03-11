ddb3437123841ab36203457ea7c71fc2ed8949a6
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-03-11 13:29:13 -0500

    Formatted prettier

--- public/labs/lab_3/script3.js
+++ public/labs/lab_3/script3.js
@@ -1,28 +1,39 @@
-const listContainer = document.querySelector('.images') /*selecting all images in code*/
+/* eslint-disable quotes */
+const listContainer = document.querySelector(".images");
+/* selecting all images in code */
 
-function shiftLeft(){
-    const lastThree = Array.from(listContainer.children).slice(4,7).reverse();
-    lastThree.forEach((element) => { /* removes the first child and then adds one */
-        listContainer.removeChild(element);
-        listContainer.insertBefore(element, listContainer.children[0]);
-    })
+function shiftLeft() {
+  const lastThree = Array.from(listContainer.children).slice(4, 7).reverse();
+  lastThree.forEach((element) => {
+    /* removes the first child and then adds one */
+    listContainer.removeChild(element);
+    listContainer.insertBefore(element, listContainer.children[0]);
+  });
 }
 
-function shiftRight(){
-    const firstThree = Array.from(listContainer.children).slice(0,3); /* saves the first three children as firstThree */
-    firstThree.forEach((element) => { /* takes first three children, removes one child and adds another */
-        listContainer.removeChild(element);
-        listContainer.appendChild(element); 
-    })
+function shiftRight() {
+  const firstThree = Array.from(listContainer.children).slice(
+    0,
+    3
+  ); /* saves the first three children as firstThree */
+  firstThree.forEach((element) => {
+    /* takes first three children, removes one child and adds another */
+    listContainer.removeChild(element);
+    listContainer.appendChild(element);
+  });
 }
 
-function onLoadOfPage(){
-    document.querySelector("button.arrow.left").addEventListener('click', (event) =>{
-        shiftLeft();
+function onLoadOfPage() {
+  document
+    .querySelector("button.arrow.left")
+    .addEventListener("click", (event) => {
+      shiftLeft();
     });
-    document.querySelector("button.arrow.right").addEventListener('click', (event) =>{
-        shiftRight();
-    })
+  document
+    .querySelector("button.arrow.right")
+    .addEventListener("click", (event) => {
+      shiftRight();
+    });
 }
 
 window.onload = onLoadOfPage;
