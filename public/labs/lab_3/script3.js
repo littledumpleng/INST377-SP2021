916dc18da7113042dc6e107722f7593c81e9b6bf
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-03-11 14:32:31 -0500

    commented script3.js

--- public/labs/lab_3/script3.js
+++ public/labs/lab_3/script3.js
@@ -1,13 +1,19 @@
+/* eslint-disable max-len */
+/* eslint-disable max-len */
 /* eslint-disable quotes */
+
+// this code forces page to reload each time you click the button. you can just use javascript and css to play with display rules
+
 const listContainer = document.querySelector(".images");
 /* selecting all images in code */
 
 function shiftLeft() {
-  const lastThree = Array.from(listContainer.children).slice(4, 7).reverse();
+  const lastThree = Array.from(listContainer.children).slice(4, 7).reverse(); // (4, 7) means 5, 6, 7. reverse makes it 7, 6, 5
   lastThree.forEach((element) => {
+    // element is 5th, 6th, or 7th image
     /* removes the first child and then adds one */
-    listContainer.removeChild(element);
-    listContainer.insertBefore(element, listContainer.children[0]);
+    listContainer.removeChild(element); // loops through elements to remove - 5, then 6, then 7
+    listContainer.insertBefore(element, listContainer.children[0]); // then puts that element at the beginning of the array
   });
 }
 
